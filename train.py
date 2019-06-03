3e88a69fc0de197eaa9e1e589c70ee13ffab1649
hdx <382619605@qq.com>
2019-06-03 14:08:28 +0800

    modify project file

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from model import build_model
 from utils import get_iterator
 
 style_pic = cv2.imread("/Users/hdx/Downloads/style1.jpg")
-style_pic = cv2.resize(style_pic, (256, 256), interpolation=cv2.INTER_LINEAR)
+style_pic = cv2.resize(style_pic, (256, 256))
 
 epoch = 5
 current_epoch = 0
@@ -20,7 +20,7 @@ MEAN_VALUES = np.array([123.68, 116.779, 103.939]).reshape((1, 1, 1, 3))
 inputs = tf.placeholder(dtype=tf.float32, shape=[None, 256, 256, 3], name="input")
 style = tf.placeholder(dtype=tf.float32, shape=[None, 256, 256, 3], name="x")
 
-iterator = get_iterator(glob.glob("/Users/hdx/code/python3/ocr/data/*.jpg"), 2, epoch)
+iterator = get_iterator(glob.glob("/Users/hdx/code/python3/ocr/data/*.png"), 2, epoch)
 optimizer, trans, total_loss = build_model(inputs, style)
 
 tf.summary.scalar('losses/total_loss', total_loss)
