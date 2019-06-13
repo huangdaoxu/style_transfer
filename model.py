df7e9f9801bb0b3ae8dd86ec5632618d1a82215c
hdx <382619605@qq.com>
2019-06-13 11:21:53 +0800

    modify project file

--- model.py
+++ model.py
@@ -53,9 +53,10 @@ def vgg_16(inputs,
     return net, end_points
 
 
-def transfer_arg_scope():
-    with slim.arg_scope([slim.conv2d, slim.conv2d_transpose],
+def transfer_arg_scope(weight_decay=0.0005):
+    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.conv2d_transpose],
                         activation_fn=None,
+                        weights_regularizer=slim.l2_regularizer(weight_decay),
                         biases_initializer=tf.zeros_initializer()):
         with slim.arg_scope([slim.conv2d, slim.conv2d_transpose], padding='SAME') as arg_sc:
             return arg_sc
